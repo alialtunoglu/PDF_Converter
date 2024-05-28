aa8e78b8fc665cce42feb5dc9f0c5fc949ed9679
Ali Altunoğlu <alialtunoglu02@gmail.com>
2024-05-28 20:14:25 +0300

    delete main.py

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-from tkinter import messagebox
-
-import text_cover
-from main import pdf_path_entry
-
-
-def convert_to_txt():
-    text_cover.pdf_to_text(pdf_path_entry.get())
-    messagebox.showinfo("Başarılı", "PDF dönüştürme işlemi tamamlandı.")
